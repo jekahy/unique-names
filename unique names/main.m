2b56ebc39e9d66235f5abbf943e2d8311eef8d2a
Mac <mahmood1@Mahmood1s-Mac.local>
2012-02-17 20:24:43 +0100

    Initial Commit

---
+++ unique names/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  unique names
+//
+//  Created by Жека Г on 2/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
