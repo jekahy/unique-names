e21e825e77b3fd17a28890b5cf593e5dc90726fe
Eugene Hyrol <jekahy@gmail.com>
2012-02-22 21:46:04 +0100

    ddd

--- unique names/main.m
+++ unique names/main.m
@@ -13,10 +13,38 @@ int main (int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSMutableArray *array=[[NSMutableArray alloc]init];       
+        
+       
+        while(true){
+            char buf[15];
+            
+            printf("Enter name:");
+            
+            gets(buf);
+            NSString *string=[NSString stringWithCString:buf encoding:NSASCIIStringEncoding];          
+            [string stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+            if(string.length==0 || string==(id)[NSNull null] || [string isEqualToString:@"(null)"] || string==nil) {
+                break; 
+            } else{  
+            if([array containsObject:string]){
+                    printf("This name is already in the base!\n");                    
+                
+                } else{
+                    [array addObject:string];  
+                    
+                    
+                }    
+            }            
+        
+        
+        }
+        printf("Unique name list:\n");      
+        NSLog(@"%@",array);      
+        
         
     }
+    
     return 0;
 }
 
